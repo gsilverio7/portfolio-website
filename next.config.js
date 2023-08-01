90d66c0c584a2dacd87cf4b17586a4735a072265
bielsil27@gmail.com <bielsil27@gmail.com>
2023-08-01 01:10:22 -0300

    changes to portifolio

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['user-images.githubusercontent.com'],
+    domains: ['user-images.githubusercontent.com', 'raw.githubusercontent.com'],
   },
   experimental: {
     appDir: false,
